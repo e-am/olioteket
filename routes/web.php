78323e5daa7b81e62282b1d15bb96a8493f9f2a6
Jennie Wahlström <jennie.wahlstrom1@gmail.com>
2018-10-16 16:20:55 +0200

    fixed routing to posts page and added folder for pages

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('posts', 'PostsController');
+Route::resource('/posts', 'PostsController');
+
+Route::get('/posts', function(){
+  return view('pages.posts');
+});
